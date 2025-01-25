e786890e222738dd629bb4f1dc61d14df6b2ea65
zohaibmemon1515 <zohaibmemon1515@gmail.com>
2025-01-25 13:15:42 +0500

    add file

--- src/sanity/env.ts
+++ src/sanity/env.ts
@@ -2,12 +2,12 @@ export const apiVersion =
   process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2025-01-17'
 
 export const dataset = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_DATASET,
+  process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
   'Missing environment variable: NEXT_PUBLIC_SANITY_DATASET'
 )
 
 export const projectId = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || ".env.local",
   'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
 )
 
