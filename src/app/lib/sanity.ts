d39babc4d5e5adbdf4f81840c2507f50ece7a8cf
zohaibmemon1515 <zohaibmemon1515@gmail.com>
2025-01-25 13:00:01 +0500

    add file

--- src/app/lib/sanity.ts
+++ src/app/lib/sanity.ts
@@ -1,10 +1,9 @@
 import { createClient } from "@sanity/client";
 
 export const client = createClient({
-  projectId: "fdxy9qun",
-  dataset: "production",
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
   apiVersion: "2025-01-17",
   useCdn: true,
-  token:
-    "skYJ8BgGumHMs0GHieJbn5pe6pIURe2FSCGRHrYYG6sdRl8X6cudFtXj4v9eZ4gDDFHcfpd27ptqEcQrdEo9xiGTr0Mx9DC8qLsn2qF7pltNbWvHlIAqOrZsZ6LxJ6DphMhwsicBrmJFkIJCJ8TBj2a36GD7LgZHWcniY4kZkBf5F40X2rVu",
+  token: process.env.NEXT_PUBLIC_SANITY_DATASET,
 });
--- src/sanity/env.ts
+++ src/sanity/env.ts
@@ -2,12 +2,12 @@ export const apiVersion =
   process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2025-01-17'
 
 export const dataset = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
+  process.env.NEXT_PUBLIC_SANITY_DATASET,
   'Missing environment variable: NEXT_PUBLIC_SANITY_DATASET'
 )
 
 export const projectId = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "fdxy9qun",
+  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
 )
 
